6b7ad93ee96f23813cff665e444a2189a94ca84e
kendalharland <kendaljharland@gmail.com>
2020-05-31 17:46:55 -0700

    Add test for github/GetBlob

--- github/github_test.go
+++ github/github_test.go
@@ -30,7 +30,7 @@ func TestGetTree(t *testing.T) {
 	}))
 	defer ts.Close()
 
-	res, err := GetTree(ts.Client(), ts.URL, "owner", "repo", "sha")
+	res, err := GetTree(ts.Client(), ts.URL, "owner", "repo", "sha", true)
 	if err != nil {
 		t.Error(err)
 	}
@@ -38,6 +38,33 @@ func TestGetTree(t *testing.T) {
 	got := res
 	want := message
 	if diff := cmp.Diff(want, got); diff != "" {
-		t.Errorf("MakeGatewayInfo() mismatch (-want +got):\n%s", diff)
+		t.Errorf("GetTree() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestGetBlob(t *testing.T) {
+	message := &GetBlobResponse{
+		SHA:      "fc6274d15fa3ae2ab983129fb037999f264ba9a7",
+		URL:      "https://api.github.com/repos/owner/repo/trees/sha",
+		Encoding: "base64",
+		Size:     5,
+		Content:  "hello",
+	}
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := json.NewEncoder(w).Encode(message); err != nil {
+			t.Fatal(err)
+		}
+	}))
+	defer ts.Close()
+
+	res, err := GetBlob(ts.Client(), ts.URL, "owner", "repo", "sha")
+	if err != nil {
+		t.Error(err)
+	}
+
+	got := res
+	want := message
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("GetBlob() mismatch (-want +got):\n%s", diff)
 	}
 }
